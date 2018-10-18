ed7f64b4fbeafac8d532480bf5a626368e7639cf
Erik Reid <erik.reid@geant.org>
2018-10-18 11:13:16 +0200

    use /grafana/version to test datasource

--- src/datasource.js
+++ src/datasource.js
@@ -100,29 +100,21 @@ export class GenericDatasource {
   }
 
   testDatasource() {
-    return Promise.resolve({
-        status: "success",
-        message: "Data source is working",
-        title: "Success"
+    var backend_request = {
+        withCredentials: this.withCredentials,
+        url: this.url + '/grafana/version',
+        method: 'GET'
+    }
+    return this.backendSrv.datasourceRequest(backend_request).then(
+        rsp => {
+            if (rsp.status === 200) {
+                return {
+                    status: "success",
+                    message: "Data source is working",
+                    title: "Success"
+                 };
+        }
     });
-
-//    var backend_request = {
-//        withCredentials: this.withCredentials,
-//        headers: this.headers,
-//        // HACK HACK: grafana removes 1 trailing slash & doesn't follow redirects
-//        url: this.url,
-//        method: 'GET'
-//    }
-//    return this.backendSrv.datasourceRequest(backend_request).then(
-//        rsp => {
-//            if (rsp.status === 200) {
-//                return {
-//                    status: "success",
-//                    message: "Data source is working",
-//                    title: "Success"
-//                 };
-//        }
-//    });
   }
 
   annotationQuery(options) {
