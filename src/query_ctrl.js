34c40db3251fa1b56d76598437dcb791d5ee1b71
Erik Reid <erik.reid@geant.org>
2018-11-27 13:15:40 +0000

    added additional query params

--- src/query_ctrl.js
+++ src/query_ctrl.js
@@ -6,20 +6,51 @@ export class GenericDatasourceQueryCtrl extends QueryCtrl {
   constructor($scope, $injector)  {
     super($scope, $injector);
 
+    this.unselected_measurement_type = 'measurement type';
+    this.unselected_participants = 'participants';
+    this.unselected_metric_type = 'metric type';
+    this.unselected_summary = 'summary'
+
     this.scope = $scope;
     this.target.measurement_type
-        = this.target.measurement_type || 'measurement types';
+        = this.target.measurement_type
+          || this.unselected_measurement_type;
+    this.target.participants
+        = this.target.participants
+          || this.unselected_participants;
+    this.target.metric_type
+        = this.target.metric_type
+          || this.unselected_metric_type;
+    this.target.summary
+        = this.target.summary
+          || this.unselected_summary;
   }
 
   getOptions(query) {
-    return this.datasource.metricFindQuery(query || '');
+    return this.datasource.metricFindQuery({
+      query: query,
+      measurement_type: this.target.measurement_type,
+      participants: this.target.participants,
+      metric_type: this.target.metric_type,
+      summary: this.target.summary
+    });
   }
 
   toggleEditorMode() {
     this.target.rawQuery = !this.target.rawQuery;
   }
 
-  onChangeInternal() {
+  onChangeInternal(option) {
+    if (option == 'measurement types') {
+      this.target.participants = this.unselected_participants;
+      this.target.metric_type = this.unselected_metric_type;
+      this.target.summary = this.unselected_summary;
+    } else if (option == 'participants') {
+      this.target.metric_type = this.unselected_metric_type;
+      this.target.summary = this.unselected_summary;
+    } else if (option == 'metric types') {
+      this.target.summary = this.unselected_summary;
+    }
     this.panelCtrl.refresh(); // Asks the panel to refresh data.
   }
 }
